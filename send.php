722cdb3abb830ecd20c4a2582edc7bdb8ebaa3ad
pblonskij48 <130553717+pblonskij48@users.noreply.github.com>
2023-04-12 13:10:55 +0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,15 @@
+В коде файлов добавлена кнопка Delete Data для её работы нужно создать ещё один файл под названием send.html и добавить туда этот код
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Remove Your Data from the Game</title>
+  </head>
+  <body>
+    <p>To remove your data from the game, please email our support team at <a href="mailto:pblonskij480@gmail.com">pblonskij480@gmail.com</a>. We will definitely review your request within 24 hours.</p>
+  </body>
+</html>
+
+
+Обязательно поменять почту отмеченную красным на почту аккаунта
